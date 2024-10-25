e85ab5a32b5f2bb768ed45384c171ed212f0f08b
kvz <kvz.box@gmail.com>
2024-10-25 17:08:54 +0300

    Add  DTO User

---
+++ data.sql
@@ -0,0 +1,29 @@
+insert into ref.level values (1, 'J', 'Junior');
+insert into ref.level values (2, 'J+', 'Junior+');
+insert into ref.level values (3, 'M', 'Middle');
+insert into ref.level values (4, 'M+', 'Middle+');
+insert into ref.level values (5, 'S', 'Senior');
+
+insert into ref.stack values (1, 'BJ', 'Backend Java');
+insert into ref.stack values (2, 'BN', 'Backend .Net');
+insert into ref.stack values (3, 'BP', 'Backend Python');
+insert into ref.stack values (4, 'FA', 'Front Angular');
+insert into ref.stack values (5, 'FR', 'Front React');
+insert into ref.stack values (6, 'BA', 'Бизнес аналитик');
+insert into ref.stack values (7, 'SA', 'Системный аналитик');
+insert into ref.stack values (8, 'EnA', 'Enterprise архитектор');
+insert into ref.stack values (9, 'SoA', 'Solution архитектор');
+insert into ref.stack values (10, 'FT', 'Функциональный тестировщик');
+insert into ref.stack values (11, 'AT', 'Авто-тестировщик');
+insert into ref.stack values (12, 'LT', 'Нагрузоный тестировщик');
+insert into ref.stack values (13, 'DO', 'DevOps');
+insert into ref.stack values (14, 'TW', 'Технический писатель');
+insert into ref.stack values (15, 'UX', 'Дизайнер');
+
+insert into ref.state values (1, '01', 'Кандидат');
+insert into ref.state values (2, '02', 'В найме');
+insert into ref.state values (3, '03', 'В штате');
+insert into ref.state values (4, '04', 'Уволен');
+
+
+
